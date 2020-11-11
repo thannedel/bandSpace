4f891b472dbb752941ca917823d8cd9013243319
thannedel <57347092+thannedel@users.noreply.github.com>
2020-11-11 14:47:48 +0100

    Profile model

---
+++ models/Profile.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+    },
+     
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  soundcloudusername: {
+    type: String
+    },
+  instagramusername: {
+    type: String
+    },
+  discography: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      releaseType: {
+        type: String,
+        required: true
+      },
+      
+      releaseDate: {
+        type: Date,
+        required: true
+      },
+      
+        description: {
+        type: String
+      }
+    }
+    ],
+  events: [
+    {
+      eventHal: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String,
+        required: true
+      },
+      
+      eventDate: {
+        type: Date,
+        required: true
+      },
+      
+      description: {
+        type: String
+      }
+    }
+    ],
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        soundcloud: {
+            type: String
+        },
+        instagram: {
+            type: String
+        },
+        bamdcamp: {
+            type: String
+        }
+    },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
